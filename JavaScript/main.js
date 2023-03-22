506bbbe0bb893aa73a11c382bb7654bd05d88cc0
davislocs <113901409+davislocs@users.noreply.github.com>
2023-03-22 23:12:12 +0200

    Initial commit
    FrontEnd mentor project

---
+++ JavaScript/main.js
@@ -0,0 +1,50 @@
+function validateForm() {
+
+let fName = document.getElementById("fName");
+let lName = document.getElementById("lName");
+let eMail = document.getElementById("eMail");
+let password = document.getElementById("password");
+
+  if(fName.value === "" || fName.value.length <= 1 || fName.value.length > 20){
+    fName.classList.remove("form__input");
+    document.getElementById("invalid-fName").style.display = "block";
+    document.getElementById("invalid-fName").textContent = "Invalid first name";
+  }
+  else{
+    fName.classList.add("form__input");
+  }
+
+  if(lName.value === "" || lName.value.length <= 1 || lName.value.length > 20){
+    lName.classList.remove("form__input");
+    document.getElementById("invalid-lName").style.display = "block";
+    document.getElementById("invalid-lName").textContent = "Invalid last name";
+  }
+  else{
+    lName.classList.add("form__input");
+  }
+
+  if(eMail.value === "" || eMail.value.length <= 4 || eMail.value.length > 30 || eMail.value.includes("@") == false || eMail.value.includes(".") == false){
+    eMail.classList.remove("form__input");
+    document.getElementById("invalid-eMail").style.display = "block";
+    document.getElementById("invalid-eMail").textContent = "Invalid Email";
+  }
+  else{
+    eMail.classList.add("form__input");
+  }
+
+  if(password.value === "" || password.value.length < 8 || password.value.length > 16){
+    password.classList.remove("form__input");
+    document.getElementById("invalid-password").style.display = "block";
+    document.getElementById("invalid-password").textContent = "Invalid pasword";
+  }
+  else{
+    password.classList.add("form__input");
+  }
+  }
+
+function resetInput(input, error){
+  input.classList.add("form__input");
+  error.style.display = "none";
+}
+
+  
